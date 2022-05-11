089e6db0090d7b7f1e268d78b23295841de1df6d
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-11 11:35:40 +0200

    Utilidadae

--- Assets/Scripts/Utilidades/Utilidades.cs
+++ Assets/Scripts/Utilidades/Utilidades.cs
@@ -8,29 +8,58 @@ using TMPro;
 
 public class Utilidades
 {
+    /// <summary>
+    /// Cabecera: public static bool comprobarCadenaVacia(string cadena)
+    /// Comentario: Este metodo se encarga de comprobar si la cadena rebidida como parametros se encuentra vacia.
+    /// Entradas: string cadena
+    /// Salidas: bool
+    /// Precondiciones: cadena debe ser distinto de null.
+    /// Postcondiciones: Se devolvera un booleano que puede tomar dos posibles valores:
+    ///                  true: Si la cadena recibida esta vacia.
+    ///                  false: Si la cadena recibida no esta vacia.
+    ///                  Si se recibide una cadena que este a null, se producira un NullPointerException.
+    /// </summary>
+    ///<param name="cadena"></param>
     public static bool comprobarCadenaVacia(string cadena)
     {
         return string.IsNullOrEmpty(cadena.Trim());
     }
 
+    /// <summary>
+    /// Cabecera: public static byte[] obtenerImagenDeUrl(string url)
+    /// Comentario: Este metodo se encarga de obtener una imagen de una url como byte[]
+    /// Entradas: string url
+    /// Salidas: byte[] foto
+    /// Precondiciones: url debera de ser una url correcta.
+    /// Postcondiciones: Se obtendra una imagen de una url como byte[].
+    /// </summary>
+    ///<param name="url"></param>
     public static byte[] obtenerImagenDeUrl(string url)
     {
-        byte[] foto;
+        byte[] imagen;
         using (WebClient webClient = new WebClient())
         {
-            foto = webClient.DownloadData(url);
+            imagen = webClient.DownloadData(url);
         }
-        return foto;
+        return imagen;
     }
 
+    /// <summary>
+    /// Cabecera: public static Sprite convertirArrayBytesASprite(byte[] imagen)
+    /// Comentario: Este metodo se encarga de convertir un byte[] en un Sprite
+    /// Entradas: byte[] imagen
+    /// Salidas: Sprite
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se obtendra un Sprite a partir de un byte[].
+    /// </summary>
+    ///<param name="imagen"></param>
     public static Sprite convertirArrayBytesASprite(byte[] imagen)
     {
-        Texture2D texture = new Texture2D(1, 1); //Texture2D necesario, porque sera donde se carge el la imagen del pokemon como byte[]
+        Texture2D texture = new Texture2D(1, 1); //Texture2D necesario, porque sera donde se carge la imagen como byte[]
         texture.LoadImage(imagen);
 
         return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(texture.width / 2, texture.height / 2));
     }
-
 
     public static void cambiarPrimerCaracterMayuscula(string cadena)
     {
@@ -89,13 +118,20 @@ public class Utilidades
         }
     }
 
-
+    /// <summary>
+    /// Cabecera: public static List<string> obtenerFrasesAleatoriasTrainer()
+    /// Comentario: Este metodo se encarga de obtener una listado de string de forma aleatoria.
+    /// Entradas: Ninguna
+    /// Salidas: List<string>
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se obtendra un List<string>
+    /// </summary>
     public static List<string> obtenerFrasesAleatoriasTrainer() { 
     List<List<string>> frasesEntrenadores = new List<List<string>> { 
         new List<string> { "Hola viajero, veo que tienes un buen equipo.", "Dejame que compruebe si es verdad." },
-        new List<string> { "¡Eh! ¡Tu equipo Pokémon me da buenas vibraciones! ", "¿Te importa que vea lo entrenado que está?" },
+        new List<string> { "Â¡Eh! Â¡Tu equipo PokÃ©mon me da buenas vibraciones! ", "Â¿Te importa que vea lo entrenado que estÃ¡?" },
         new List<string> { "Te voy hacer comer polvo.", "Para que te hagas una idea, como el que va a comer Santi con el proyecto final." },
-        new List<string> { "¿Qué necesita mi vista?"},
+        new List<string> { "Â¿QuÃ© necesita mi vista?"},
         new List<string> { "F1", "." },
         new List<string> { "F2", "." },
         new List<string> { "F3", "." },
@@ -108,11 +144,19 @@ public class Utilidades
         return frasesEntrenadores[Random.Range(0, frasesEntrenadores.Count)];
     }
 
+    /// <summary>
+    /// Cabecera: public static List<string> obtenerFrasesAleatoriasTrainerDerrotado()
+    /// Comentario: Este metodo se encarga de obtener una listado de string de forma aleatoria.
+    /// Entradas: Ninguna
+    /// Salidas: List<string>
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se obtendra un List<string>
+    /// </summary>
     public static List<string> obtenerFrasesAleatoriasTrainerDerrotado()
     {
         List<List<string>> frasesEntrenadoresDerotao = new List<List<string>> {
         new List<string> { "Pues si que eres fuerte."},
-        new List<string> { "Pufff que combate más bueno, he aprendido mucho.", "Espero que volvamos a combatir otra vez." },
+        new List<string> { "Pufff que combate mÃ¡s bueno, he aprendido mucho.", "Espero que volvamos a combatir otra vez." },
         new List<string> { "Que sepas que me he dejado.", "No quiero abusar de un crio." },
         new List<string> { "...","..."},
         new List<string> { "FD1", "." },
