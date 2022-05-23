117b5aaac9d2d92a981e1e541db37126f4b3633d
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-23 11:40:06 +0200

    Documentacion ConfiguracionObjectoInteractable

--- Assets/Scripts/Scripts/Extras/ConfiguracionObjectoInteractable.cs
+++ Assets/Scripts/Scripts/Extras/ConfiguracionObjectoInteractable.cs
@@ -12,12 +12,13 @@ public class ConfiguracionObjectoInteractable : MonoBehaviour
 
     private void Update()
     {
+        //Si el jugador esta dentro del rango, pulsa la tecla E y no hay un dialogo iniciado 
         if (jugadorDentroRango && Input.GetKey(KeyCode.E) && PlayerPrefs.GetString("EstadoDialogo") == DialogEstate.END.ToString())
         {
-            PlayerPrefs.SetString("InteraccionConObjeto", gameObject.tag);
+            PlayerPrefs.SetString("InteraccionConObjeto", gameObject.tag);//Se guarda el tipo de objeto con el que sea interactuado
             GameObject dialogo = Resources.FindObjectsOfTypeAll<GameObject>().First(g => g.name == "CanvasDialogo");
             ControlDialogos controlDialogos = dialogo.transform.GetChild(0).gameObject.GetComponent<ControlDialogos>();
-            if (gameObject.tag == "Trainer") {
+            if (gameObject.tag == "Trainer") { //Si se interactua con un entrenador
                 TrainerNPC trainer = gameObject.transform.parent.gameObject.transform.GetChild(0).GetComponentInChildren<TrainerNPC>();
                 if (trainer.derrotado)
                 {
@@ -32,28 +33,39 @@ public class ConfiguracionObjectoInteractable : MonoBehaviour
                 controlDialogos.ListaFrases = frases.ToArray();
             }
 
-            if (gameObject.tag == "Objeto") {
+            if (gameObject.tag == "Objeto") { //Si se trara de un objeto, se destruye ya que se suma al inventario del jugador
                 Destroy(gameObject);
             }
+            //Se muestra la interfaz del dialogo y se activa
             dialogo.SetActive(true);
             dialogo.transform.GetChild(0).gameObject.SetActive(true);//La imagen del dialogo
             controlDialogos.activarDialogo();
         }
     }
 
+    //Metodo para controlar cuando el jugador entra dentro de la zona de iteracion que tiene asociado un objeto que es interactable
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !collision.isTrigger ) {
             jugadorDentroRango = true;
         }
     }
-
+    //Metodo para controlar cuando el jugador sale de la zona de iteracion que tiene asociado un objeto que es interactable
     private void OnTriggerExit2D(Collider2D collision)
     {
         jugadorDentroRango = false;
     }
 
-    public IEnumerator activarExclamacionTrainerCombate() {
+    /// <summary>
+    /// Cabecera: IEnumerator activarExclamacionTrainerCombate()
+    /// Comentario: Esta corrutina se encarga de activar la imagen de exclamacion que tiene un entrenador
+    /// Entradas: Ninguna
+    /// Salidas: Ninguna
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se activara la imagen de una exclacion duranto un breve periodo de tiempo y despues se desactivara
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator activarExclamacionTrainerCombate() {
         GameObject iconoExclamacion = gameObject.transform.parent.gameObject.transform.GetChild(1).gameObject;
         iconoExclamacion.SetActive(true);
         yield return new WaitForSeconds(1.5f);
