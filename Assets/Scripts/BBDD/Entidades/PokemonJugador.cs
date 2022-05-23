5f1c5fc725a4d83f938ac5e307125615cd3f7e82
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-23 13:08:09 +0200

    Documentacion PokemonJugador

--- Assets/Scripts/BBDD/Entidades/PokemonJugador.cs
+++ Assets/Scripts/BBDD/Entidades/PokemonJugador.cs
@@ -8,7 +8,9 @@ public class PokemonJugador : Pokemon
     private int experiencia;
     private int experienciaSiguienteNivel = 1;
     #endregion
-
+    
+    #region Atributos
+    //Constructor sin parametros
     public PokemonJugador() : base()
     {
         IDJugador = 0;
@@ -16,7 +18,7 @@ public class PokemonJugador : Pokemon
         NumeroEquipado = 0;
         experiencia = 0;
     }
-
+    //Constructor con parametros
     public PokemonJugador(Pokemon pokemon, int idJugador, int pokemonNumero, int numeroEquipado, int experiencia) : base(pokemon.ID, pokemon.Nombre, pokemon.HP, pokemon.Nivel, pokemon.Ataque, pokemon.Defensa, pokemon.Velocidad, pokemon.Movimientos, pokemon.Tipos, pokemon.Debilidades)
     {
         IDJugador = idJugador;
@@ -24,7 +26,9 @@ public class PokemonJugador : Pokemon
         NumeroEquipado = numeroEquipado;
         Experiencia = experiencia;
     }
-
+    #endregion 
+    
+    #region Metodos Fundamentales(Propiedades)
     //IdJugador
     public int IDJugador { get; }
     //PokemonNumero
@@ -43,22 +47,20 @@ public class PokemonJugador : Pokemon
             }
         }
     }
+    #endregion 
 
-
-
+    #region Metodos añadidos
     /// <summary>
-    /// 
+    /// Cabecera: public bool comprobarSubirNivel()
+    /// Comentario: Este metodo se encarga 
+    /// Entradas: Ninguna
+    /// Salidas: bool subidaNivel
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se devolvera un booleano cuyo valor sera:
+    ///                  true: Si el pokemon a subido de nivel 
+    ///                  flase: Si el pokemon no ha subido de nivel
     /// </summary>
-    /// <param name="experiencia"></param>
-    public void aumentarExperiencia(int experiencia)
-    {
-        this.experiencia += experiencia;
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
+    /// <returns>bool</returns>
     public bool comprobarSubirNivel()
     {
         bool subidaNivel = false;
@@ -67,16 +69,18 @@ public class PokemonJugador : Pokemon
         {
             subidaNivel = true;
             Nivel += 1; //De esta manera se entra en set de Nivel y se controlara que el nivel no pase de 100
-            experiencia = 0;//experienciaSiguienteNivel; //Se resetea la experiencia del pokemon
+            experiencia = 0;//Se resetea la experiencia del pokemon
             experienciaSiguienteNivel += 100; //Aumenta la experiencia requerida para el siguiente nivel
         }
         return subidaNivel;
     }
-
+    #endregion
+    
+    #region Metodos Heredados
+    //Criterio de igualadad: Que los id y los pokemonNumero de los pokemons sean iguales
     public override bool Equals(object obj)
     {
         bool iguales = false;
-
         if (this == obj)
         {
             iguales = true;
@@ -90,4 +94,5 @@ public class PokemonJugador : Pokemon
         }
         return iguales;
     }
+    #endregion
 }
