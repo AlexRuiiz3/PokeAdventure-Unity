592b70975cc59dd5e129c7f346fc677b989e54ea
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-23 11:28:51 +0200

    Sumarys ControlDialogos

--- Assets/Scripts/Scripts/Dialogos/ControlDialogos.cs
+++ Assets/Scripts/Scripts/Dialogos/ControlDialogos.cs
@@ -5,14 +5,23 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum DialogEstate { PLAY,END}
-public class ControlDialogos : MonoBehaviour //Script que ira en el gameObejct que sera donde se muestre el dialogo
+public enum DialogEstate {PLAY,END}
+public class ControlDialogos : MonoBehaviour //Script que ira en el gameObject que sera donde se muestre el dialogo
 {
     public Queue<string> colaTextos = new Queue<string>();
     public string[] ListaFrases;
     [SerializeField] public TextMeshProUGUI textoPantalla;
     private bool reproduciendoTexto;
 
+     /// <summary>
+    /// Cabecera: public void activarDialogo()
+    /// Comentario: Este metodo se encarga de preparar e iniciar un dialogo
+    /// Entradas: Ninguna
+    /// Salidas: Ninguna
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se preparara e iniciara un dialogo
+    /// </summary>
+    /// <returns></returns>
     public void activarDialogo()
     {
         GameObject jugador = GameObject.Find("Player");
@@ -32,6 +41,16 @@ public class ControlDialogos : MonoBehaviour //Script que ira en el gameObejct q
         siguienteFrase();
     }
 
+    /// <summary>
+    /// Cabecera: public void siguienteFrase()
+    /// Comentario: Este metodo se encarga de avanzar a la siguiente frase que haya en un dialogo.
+    /// Entradas: Ninguna
+    /// Salidas: Ninguna
+    /// Precondiciones: Ningnua
+    /// Postcondiciones: Se pasara a la siguiente frase de un dialogo. Si ya no hay mas frases y sea terminado el dialogo, se realizara una accion de fin de dialogo.
+    /// </summary>
+    /// <param name="nombreUsuario"></param>
+    /// <returns></returns>
     public void siguienteFrase()
     {
         if (colaTextos.Count != 0 && !reproduciendoTexto)
@@ -49,6 +68,16 @@ public class ControlDialogos : MonoBehaviour //Script que ira en el gameObejct q
         }
     }
 
+    /// <summary>
+    /// Cabecera: IEnumerator mostrarTextoPorCaracteres(string frase)
+    /// Comentario: Esta corrutina se encarga del texto que se muestra por pantalla configurarlo para que se muestre caracter a caracter.
+    /// Entradas: string frase
+    /// Salidas: Ninguna
+    /// Precondiciones: frase debe ser distinto a null
+    /// Postcondiciones: Se mostrara en pantalla una frase caracter por caracter.
+    /// </summary>
+    /// <param name="frase"></param>
+    /// <returns></returns>
     IEnumerator mostrarTextoPorCaracteres(string frase)
     {
         foreach (char caracter in frase.ToCharArray())
