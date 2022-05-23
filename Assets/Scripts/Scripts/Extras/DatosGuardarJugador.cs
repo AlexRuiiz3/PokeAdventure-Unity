d8c9e444e17a1501f34a7c5bd5b3a4069d74e9c4
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-23 11:48:46 +0200

    Documentacion DatosGuardarJugador

--- Assets/Scripts/Scripts/Extras/DatosGuardarJugador.cs
+++ Assets/Scripts/Scripts/Extras/DatosGuardarJugador.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
+/*
+* Clase que sirve para tener almacenados de manera global y rapida datos del jugador. Su principal finalidad es la de guardar datos del jugador pero sin pasar por la base de datos, 
+* solo se guardaran en la base de datos cuando el jugador realice la funcion de Guardar Datos, entonces el contenido de esta clase sera obtenido y guardado en la base de datos. 
+* De esta manera se puede conseguir la funcionalidad de si cierras el juego y no guardas, pierdes todo lo nuevo conseguido.
+*/
 public class DatosGuardarJugador
 {
 
