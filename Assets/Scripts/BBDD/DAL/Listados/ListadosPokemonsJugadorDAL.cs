6752599a9e78efc912cfa377f2300cc39bf8df7f
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-23 10:54:12 +0200

    Sumarys ListadosPokemonsJugadorDAL

--- Assets/Scripts/BBDD/DAL/Listados/ListadosPokemonsJugadorDAL.cs
+++ Assets/Scripts/BBDD/DAL/Listados/ListadosPokemonsJugadorDAL.cs
@@ -8,7 +8,17 @@ using UnityEngine;
 
 public class ListadosPokemonsJugadorDAL
 {
-
+    /// <summary>
+    /// Cabecera: public static List<PokemonJugador> obtenerPokemonsNoEquipadosJugador(int id)
+    /// Comentario: Este metodo se encarga de obtener los pokemons de un jugador que no tiene equipados.
+    /// Entradas: int id
+    /// Salidas: List<PokemonJugador> pokemonsJugador
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se devolvera una lista con objetos de tipo PokemonJugador. Si se produce una excepcion o no se encuentra un jugador con el id 
+    ///                  recibido o la consulta no tiene resultados, la lista devuelta estara vacia. 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>List<PokemonJugador></returns>
     public static List<PokemonJugador> obtenerPokemonsNoEquipadosJugador(int id) 
     {
         List<PokemonJugador> pokemonsJugador = new List<PokemonJugador>();
@@ -30,13 +40,15 @@ public class ListadosPokemonsJugadorDAL
                 List<string> debilidadesPokemon = new List<string>();
                 while (reader.Read())
                 {
+                    //Se obtienen los movimientos del pokemon
                     movimientosPokemon = ListadosMovimientoBL.obtenerMovimientosPokemon(reader.GetInt32(0), reader.GetInt32(1), reader.GetInt32(2));
+                    //Se obtienen los tipos del pokemon
                     tiposPokemon = ListadosTipoBL.obtenerTiposPokemon(reader.GetInt32(1));
+                    //Por cada tipo del pokemon se obtienen sus tipos debiles
                     foreach (string tipo in tiposPokemon) {
                         debilidadesPokemon.Concat(ListadosTipoBL.obtenerTiposDebilesTipo(tipo));
                     }
                     debilidadesPokemon = debilidadesPokemon.Distinct().ToList();//Se eliminan los tipos repetidos que existan.Por ejemplo pokemon tipo planta e hielo tendria duplicado el tipo debilad fuego, se elimina ya que ocuparia un espacio innecesario
-                    
                     
                     pokemonsJugador.Add(new PokemonJugador(
                         new Pokemon(reader.GetInt32(1), reader.GetString(3), reader.GetInt32(4),
@@ -48,7 +60,6 @@ public class ListadosPokemonsJugadorDAL
         }
         catch (Exception)
         {
-            Debug.Log("Error en la obtencion de los pokemons no equipados");
         }
         finally
         {
@@ -61,6 +72,17 @@ public class ListadosPokemonsJugadorDAL
         return pokemonsJugador;
     }
     
+    /// <summary>
+    /// Cabecera: public static List<PokemonJugador> obtenerPokemonsJugadorEquipados(int id)
+    /// Comentario: Este metodo se encarga de obtener los pokemons de un jugador que tiene equipados.
+    /// Entradas: int id
+    /// Salidas: List<PokemonJugador> pokemonsJugador
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se devolvera una lista con objetos de tipo PokemonJugador. Si se produce una excepcion o no se encuentra un jugador con el id 
+    ///                  recibido o la consulta no tiene resultados, la lista devuelta estara vacia. 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>List<PokemonJugador></returns>
     public static List<PokemonJugador> obtenerPokemonsJugadorEquipados(int id)
     {
         List<PokemonJugador> pokemonsJugador = new List<PokemonJugador>();
@@ -100,9 +122,6 @@ public class ListadosPokemonsJugadorDAL
         }
         catch (Exception)
         {
-            Debug.Log("Error en la obtencion de los pokemons equipados");
-            throw;
-            
         }
         finally
         {
@@ -115,6 +134,17 @@ public class ListadosPokemonsJugadorDAL
         return pokemonsJugador;
     }
 
+    /// <summary>
+    /// Cabecera: public static int obtenerNumeroPokemonsJugador(int id)
+    /// Comentario: Este metodo se encarga de obtener el numero total de pokemons que tiene un jugador en especifico
+    /// Entradas: int id
+    /// Salidas: int numeroPokemons
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se devolvera un entero con el numero de pokemons que tiene un jugador. Si se produce una excepcion o no se encuentra un jugador con el id 
+    ///                  recibido o la consulta no tiene resultados, se devolvera 0. 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>int</returns>
     public static int obtenerNumeroPokemonsJugador(int id)
     {
         int numeroPokemons = 0;
@@ -137,8 +167,6 @@ public class ListadosPokemonsJugadorDAL
         }
         catch (Exception)
         {
-            Debug.Log("Error en la obtencion del numero de pokemons del jugador");
-            throw;
         }
         finally
         {
@@ -150,6 +178,4 @@ public class ListadosPokemonsJugadorDAL
         }
         return numeroPokemons;
     }
-
 }
-
