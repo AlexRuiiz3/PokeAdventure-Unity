3703f70af78d228febc3512bc6a2051f017ce3f7
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-24 09:35:26 +0200

    Sumarys ListadosPokemonEncontradosJugadorBL

--- Assets/Scripts/BBDD/BL/Listados/ListadosPokemonEncontradosJugadorBL.cs
+++ Assets/Scripts/BBDD/BL/Listados/ListadosPokemonEncontradosJugadorBL.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 public class ListadosPokemonEncontradosJugadorBL
 {
-    public static List<PokemonEncontrado> obtenerPokemonsEncontradosDeJugador(int nombreUsuario)
+    /// <summary>
+    /// Cabecera: public static List<PokemonEncontrado> obtenerPokemonsEncontradosDeJugador(int id)
+    /// Comentario: Este metodo se encarga de llamar al metodo obtenerPokemonsEncontradosDeJugador de la clase ListadosPokemonEncontradosJugadorDAL de la capa DAL.
+    /// Entradas: in id
+    /// Salidas: List<PokemonEncontrado> 
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se devolvera una lista de objetos PokemonEncontrado. Si se produce una excepcion, 
+    ///                  el id no corresponde con el de ningun jugador o la consulta no tiene resultado, la lista devuelta estara vacia.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>List<PokemonEncontrado></returns>
+    public static List<PokemonEncontrado> obtenerPokemonsEncontradosDeJugador(int id)
     {
-        return ListadosPokemonEncontradosJugadorDAL.obtenerPokemonsEncontradosDeJugador(nombreUsuario);
+        return ListadosPokemonEncontradosJugadorDAL.obtenerPokemonsEncontradosDeJugador(id);
     }
+    
+    /// <summary>
+    /// Cabecera: public static int obtenerNumeroPokemonsEncontradosDeJugador(int id)
+    /// Comentario: Este metodo se encarga de llamar al metodo obtenerNumeroPokemonsEncontradosDeJugador de la clase ListadosPokemonEncontradoJugadorDAL de la capa DAL.
+    /// Entradas: int id
+    /// Salidas: int 
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se devolvera un entero que representa el numero de pokemons que un jugador sea encontrado. Si se produce una excepcion, 
+    ///                  el id no corresponde con el de ningun jugador o la consulta no tiene resultado, se devolvera 0.
+    /// </summary>
+    /// <param name="nombreUsuario"></param>
+    /// <returns></returns>
     public static int obtenerNumeroPokemonsEncontradosDeJugador(int id)
     {
         return ListadosPokemonEncontradosJugadorDAL.obtenerNumeroPokemonsEncontradosDeJugador(id);
