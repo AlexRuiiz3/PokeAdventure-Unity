50b5aed5639a098c07af3e0530cf5679fe79fa04
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-24 10:03:03 +0200

    Documentacion Jugador

--- Assets/Scripts/Modelos/Jugador.cs
+++ Assets/Scripts/Modelos/Jugador.cs
@@ -1,13 +1,40 @@
+/*
+ * Clase: Jugador
+ * 
+ * Comentario: Esta clase representa a un jugador dentro de una partida que cuenta con un equipo pokemon y una mochila de items. Hereda de la clase ClsJugador.
+ * 
+ * Atributos:
+ *          Basicos:
+ *              equipoPokemon: Listado, Consultable.
+ *              mochila: Listado, Consultable.
+ *          Derivados: Ninguno.
+ *          Compartidos: Ninguno.
+ *          
+ * Metodos fundamentales(Propiedades)
+ *              EquipoPokemon: public List<PokemonJugador> EquipoPokemon { get; }
+ *              Mochila: public List<ItemConCantidad> Mochila { get; }
+ *
+ * Metodos añadidos: Ninguno.
+ * 
+ * Metodos heredados: Ninguno.
+ * 
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Jugador : ClsJugador
 {
+    #region Constructores
+    //Constructor con parametros
     public Jugador(ClsJugador jugador, List<PokemonJugador> equipoPokemon, List<ItemConCantidad> mochila ) : base(jugador.ID, jugador.NombreUsuario, jugador.Contrasenha, jugador.Contrasenha, jugador.Dinero, jugador.Foto) {
         EquipoPokemon = equipoPokemon;
         Mochila = mochila;
     }
-    public List<PokemonJugador> EquipoPokemon { get; set; }
-    public List<ItemConCantidad> Mochila { get; set; }
+    #endregion
+    
+    #region Metodos Fundamentales(Propiedades)
+    public List<PokemonJugador> EquipoPokemon { get; }
+    public List<ItemConCantidad> Mochila { get; }
+    #endregion
 }
