3cdc563404ab5fe3632aba6379d50fcc58c70a2d
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-24 09:14:01 +0200

    Sumarys ListadosItemBL

--- Assets/Scripts/BBDD/BL/Listados/ListadosItemBL.cs
+++ Assets/Scripts/BBDD/BL/Listados/ListadosItemBL.cs
@@ -6,18 +6,64 @@ using System.Threading.Tasks;
 
 public class ListadosItemBL
 {
+    /// <summary>
+    /// Cabecera: public static Item obtenerItemAleatorio()
+    /// Comentario: Este metodo se encarga de obtener de la base de datos un objeto de tipo Item de forma aleatoria
+    /// Entradas: Ninguna
+    /// Salidas: Item item
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se devolvera un objeto de tipo Item de la base de datos de forma aleatoria.
+    ///                  Si se produce alguna excepcion se devolvera un objeto de tipo Item con los valores por defecto.
+    /// </summary>
+    /// <returns>Item</returns>
     public static Item obtenerItemAleatorio()
     {
         return ListadosItemDAL.obtenerItemAleatorio();
     }
+    
+    /// <summary>
+    /// Cabecera: public static List<Item> obtenerItems()
+    /// Comentario: Este metodo se encarga de obtener todos los items que existan en la base de datos.
+    /// Entradas: Ninguna
+    /// Salidas: List<Item> item
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se devolvera una lista de objetos de tipo Item que existen en la base de datos.
+    ///                  Si se produce alguna excepcion se devolvera una lista de Item vacia.
+    /// </summary>
+    /// <returns>List<Item></returns>
     public static List<Item> obtenerItems()
     {
         return ListadosItemDAL.obtenerItems();
     }
+    
+    /// <summary>
+    /// Cabecera: public static List<ItemConCantidad> obtenerItemsJugador(int id)
+    /// Comentario: Este metodo se encarga de obtener todos los items que tiene asociados un jugador.
+    /// Entradas: int id
+    /// Salidas: List<ItemConCantidad> items
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se devolvera una lista de objetos de tipo ItemConCantidad que tiene un jugador especifico.
+    ///                  Si se produce alguna excepcion o el id recibido no coincide con el de ningun jugador o el jugador no tiene ningun item asignado 
+    ///                  se devolvera una lista de Item vacia.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>List<ItemConCantidad></returns>
     public static List<ItemConCantidad> obtenerItemsJugador(int id)
     {
         return ListadosItemDAL.obtenerItemsJugador(id);
     }
+    
+    /// <summary>
+    /// Cabecera: public static Item obtenerItem(int id)
+    /// Comentario: Este metodo se encarga de obtener un item especifico de la base de datos.
+    /// Entradas: int id
+    /// Salidas: Item item
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se devolvera una lista de objetos de tipo ItemConCantidad que tiene un jugador especifico.
+    ///                  Si se produce alguna excepcion o no se encuentra ningun item con el id recibido, se devolvera un item con los valores por defecto.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Item</returns>
     public static Item obtenerItem(int id)
     {
         return ListadosItemDAL.obtenerItem(id);
