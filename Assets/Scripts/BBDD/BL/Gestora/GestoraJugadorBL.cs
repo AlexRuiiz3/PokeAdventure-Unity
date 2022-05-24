b61ab889fbd3af0b188046a51947febf531cb1d3
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-24 09:43:31 +0200

    Sumary GestoraJugadorBL

--- Assets/Scripts/BBDD/BL/Gestora/GestoraJugadorBL.cs
+++ Assets/Scripts/BBDD/BL/Gestora/GestoraJugadorBL.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class GestoraJugadorBL
 {
+    /// <summary>
+    /// Cabecera: public static void insertarJugador(ClsJugador jugador)
+    /// Comentario: Este metodo se encarga de llamar al metodo insertarJugador de la clase GestoraJugadorDAL de la capa DAL.
+    /// Entradas: ClsJugador jugador
+    /// Salidas: Ninguna
+    /// Precondiciones: jugador no debera ser distinto de null
+    /// Postcondiciones: Se insertara en la base de datos un nuevo jugador. Si se produce algun tipo de excepcion el jugador no se einsertara en la base de datos.
+    /// </summary>
+    /// <param name="jugador"></param>
+    /// <returns></returns>
     public static void insertarJugador(ClsJugador jugador) 
     {
         GestoraJugadorDAL.insertarJugador(jugador);
