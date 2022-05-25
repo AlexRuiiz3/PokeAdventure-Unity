4d56e57f52fdd70cc38f44040f578ff7e28fb1ca
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-25 12:32:49 +0200

    Sumarys + documentacion MenuEquipo

--- Assets/Scripts/Scripts/Menus/Menu Jugador/MenuEquipoPokemon.cs
+++ Assets/Scripts/Scripts/Menus/Menu Jugador/MenuEquipoPokemon.cs
@@ -12,19 +12,24 @@ public class MenuEquipoPokemon : MonoBehaviour
     private GameObject interfazPokemonSeleccionado;
     private PokemonJugador pokemonSeleccionado;
 
+    /// <summary>
+    /// Cabecera: public void prepararMenuEquipo(GameObject plantillaInterfazPokemon)
+    /// Comentario: Este metodo se encarga configurar y activar el menu de equipo del jugador.
+    /// Entradas: GameObject plantillaInterfazPokemon
+    /// Salidas: Ninguna
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se mostrara el menu del equipo del jugador preparado con los pokemons que el jugador tenga en su equipo.
+    /// </summary>
+    /// <param name="plantillaInterfazPokemon"></param>
     public void prepararMenuEquipo(GameObject plantillaInterfazPokemon) { 
-        GameObject interfazPokemon = null, contentPokemons;
+        GameObject interfazPokemon, 
+                   contentPokemons = gameObject.transform.Find("ContentPokemons").gameObject;
         PokemonJugador pokemon;
         jugador = GameObject.Find("Player").GetComponent<PlayerController>().Jugador;
 
-        //Eliminar
-        jugador.EquipoPokemon[0].HPMaximos = 100;
-        jugador.EquipoPokemon[0].HP = 45;
-
-        contentPokemons = gameObject.transform.Find("ContentPokemons").gameObject;
-        UtilidadesEscena.eliminarHijosGameObject(contentPokemons);
+        UtilidadesEscena.eliminarHijosGameObject(contentPokemons);//Si se accede al menu y ya hay algo, se eliminan, evitando asi que se dupliquen los pokemon del equipo
         gameObject.GetComponentsInChildren<TextMeshProUGUI>()[1].text = $"Equipo Actual {jugador.EquipoPokemon.Count}/6";
-        for (int i = 0; i < jugador.EquipoPokemon.Count; i++)//jugador.EquipoPokemon.Count
+        for (int i = 0; i < jugador.EquipoPokemon.Count; i++)
         {
             pokemon = jugador.EquipoPokemon[i];
             interfazPokemon = Instantiate(plantillaInterfazPokemon);
@@ -35,18 +40,27 @@ public class MenuEquipoPokemon : MonoBehaviour
             interfazPokemon.GetComponentsInChildren<TextMeshProUGUI>()[1].text = $"PS: {pokemon.HP} / {pokemon.HPMaximos}";
             interfazPokemon.GetComponentsInChildren<TextMeshProUGUI>()[2].text = $"Nvl.{pokemon.Nivel}";
 
-            interfazPokemon.transform.SetParent(contentPokemons.transform);
+            interfazPokemon.transform.SetParent(contentPokemons.transform); //Se agrega la interfaz del pokemon creada al content del scrollView
             interfazPokemon.transform.localScale = new Vector3(1,1,1);
             interfazPokemon.SetActive(true);
         }
-        if (interfazPokemon != null && jugador.EquipoPokemon.Count < 2) //Cambiar a < 2 
+        if (jugador.EquipoPokemon.Count < 2)//Si el jugador tiene solo un pokemon, del menu de opciones del pokemon se desactiva el boton de cambiar posicion
         {
           Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(g => g.name == "MenuOpcionesPokemon").transform.Find("ButtonCambiarPosicion").gameObject.GetComponent<Button>().interactable = false;
-
         }
         gameObject.SetActive(true);
     }
 
+    /// <summary>
+    /// Cabecera: public void mostrarConfigurarMenuOpcionesPokemon(GameObject interfazPokemon)
+    /// Comentario: Este metodo se encarga de configurar y mostrar el menu con las opciones principales que se pueden realizar sobre un pokemon. 
+    ///             Ademas guarda el pokemon que sea seleccionado y su interfaz.
+    /// Entradas: GameObject interfazPokemon
+    /// Salidas: Ninguna
+    /// Precondiciones: interfazPokemon no debe estar a null(Sino se producira un NullPointerException)
+    /// Postcondiciones: Se mostrara un menu que cuenta con las principales operaciones que se realizan sobre un pokemon y se guadara el pokemon seleccionado y su interfaz.
+    /// </summary>
+    /// <param name="interfazPokemon"></param>
     public void mostrarConfigurarMenuOpcionesPokemon(GameObject interfazPokemon) {
         GameObject menuOpciones = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(g => g.name == "MenuOpcionesPokemon");
         interfazPokemonSeleccionado = interfazPokemon;
@@ -55,44 +69,57 @@ public class MenuEquipoPokemon : MonoBehaviour
         menuOpciones.SetActive(true);
     }
 
+    /// <summary>
+    /// Cabecera: public void opcionVerDatos(GameObject menuDatos)
+    /// Comentario: Este metodo se encarga de recoger los datos necesarios para poder llamar al metodo configurarMenuDatosPokemon de la clase UtilidadesEscena.
+    /// Entradas: GameObject menuDatos
+    /// Salidas: Ninguna
+    /// Precondiciones: menuDatos no debe estar a null(Sino se producira un NullPointerException)
+    /// Postcondiciones: El menu de datos esta configurado con los valores del pokemon seleccionado
+    /// </summary>
+    /// <param name="menuDatos"></param>
     public void opcionVerDatos(GameObject menuDatos) {
-        menuDatos.GetComponentsInChildren<Image>()[1].sprite = Resources.LoadAll<Sprite>("Imagenes/Pokemons/Front/" + pokemonSeleccionado.ID).First();
-        menuDatos.GetComponentsInChildren<TextMeshProUGUI>()[0].text = pokemonSeleccionado.Nombre;
-        menuDatos.GetComponentsInChildren<TextMeshProUGUI>()[1].text = $"Nvl.{pokemonSeleccionado.Nivel}";
-        menuDatos.GetComponentsInChildren<TextMeshProUGUI>()[2].text = $"PS: {pokemonSeleccionado.HP}/{pokemonSeleccionado.HPMaximos}";
-
-        GameObject movimientos = menuDatos.transform.Find("Movimientos").gameObject, movimientoInterfaz;
-        MovimientoPokemon movimientoPokemon;
-        for (int i = 1; i < movimientos.transform.childCount; i++) { //Empieza en 1 porque ese hijo no es un movimiento sino un titulo
-            movimientoPokemon = pokemonSeleccionado.Movimientos[i - 1];
-            movimientoInterfaz = movimientos.transform.GetChild(i).gameObject;
-            movimientoInterfaz.GetComponent<Image>().sprite = Resources.LoadAll<Sprite>("Imagenes/UI/Tipos/Banners/" + movimientoPokemon.Tipo)[0]; ;
-            movimientoInterfaz.GetComponentsInChildren<TextMeshProUGUI>()[0].text = movimientoPokemon.Nombre;
-            movimientoInterfaz.GetComponentsInChildren<TextMeshProUGUI>()[1].text = $"Potencia: {movimientoPokemon.Danho}";
-            movimientoInterfaz.GetComponentsInChildren<TextMeshProUGUI>()[2].text = $"Preicion: {movimientoPokemon.Precicion}";
-            movimientoInterfaz.GetComponentsInChildren<TextMeshProUGUI>()[3].text = $"PP {movimientoPokemon.PP}/{movimientoPokemon.PPMaximo}";
-        }
-        menuDatos.SetActive(true);
+        UtilidadesEscena.configurarMenuDatosPokemon(menuDatos, pokemonSeleccionado);
     }
 
+    /// <summary>
+    /// Cabecera: public void buttonAceptarCambiarNombre(Text input)
+    /// Comentario: Este metodo se encarga de cambiar el nombre a un pokemon seleccionado con el valor recibido por parametros. 
+    /// Entradas: Text input
+    /// Salidas: Ninguna
+    /// Precondiciones: input no debe estar a null(Sino se producira un NullPointerException)
+    /// Postcondiciones: Se modificara el nombre del pokemon seleccionado. 
+    ///                  Si el texto del valor recibido esta vacio el nombre no se modifica y se informa al jugador de ello.
+    /// </summary>
+    /// <param name="input"></param>
     public void buttonAceptarCambiarNombre(Text input) {
         if (!Utilidades.comprobarCadenaVacia(input.text))
         {
             interfazPokemonSeleccionado.GetComponentsInChildren<TextMeshProUGUI>()[0].text = input.text;
             GameObject menuOpciones = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(g => g.name == "MenuOpcionesPokemon");
             menuOpciones.GetComponentInChildren<TextMeshProUGUI>().text = input.text;
+            
             pokemonSeleccionado.Nombre = input.text;
-            Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(g => g.name == "MenuCambiarNombre").SetActive(false);
+            EventSystem.current.currentSelectedGameObject.transform.parent.gameObject.SetActive(false); 
         }
         else {
             UtilidadesEscena.mostrarMensajeError("El nombre no puede estar vacio");
         }
     }
 
+    /// <summary>
+    /// Cabecera: public void mostrarConfigurarMenuCambiarPosicion(GameObject plantillaPokemonCambiar)
+    /// Comentario: Este metodo se encarga de configurar y activar el menu que permite al jugador intercambiar la posicion entre dos pokemons.
+    /// Entradas: GameObject plantillaPokemonCambiar
+    /// Salidas: Ninguna
+    /// Precondiciones: plantillaPokemonCambiar no debe estar a null(Sino se producira un NullPointerException)
+    /// Postcondiciones: Se mostrara un menu con botones por cada pokemon que tenga el jugador en su equipo.
+    /// </summary>
+    /// <param name="plantillaPokemonCambiar"></param>
     public void mostrarConfigurarMenuCambiarPosicion(GameObject plantillaPokemonCambiar) {
         GameObject menuCambiarPosicion = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(g => g.name == "MenuCambiarPosicion"),
-            content = menuCambiarPosicion.transform.Find("Content").gameObject,
-            interfazPokemonCambiar;
+                   content = menuCambiarPosicion.transform.Find("Content").gameObject,
+                   interfazPokemonCambiar;
         UtilidadesEscena.eliminarHijosGameObject(content);
         foreach (PokemonJugador pokemon in jugador.EquipoPokemon) {
             interfazPokemonCambiar = Instantiate(plantillaPokemonCambiar);
@@ -110,6 +137,15 @@ public class MenuEquipoPokemon : MonoBehaviour
         menuCambiarPosicion.SetActive(true);
     }
 
+    /// <summary>
+    /// Cabecera: public void aplicarCambioPosicionPokemons(GameObject interfazPokemonCambiar)
+    /// Comentario: Este metodo se encarga de cambiar las posicion entre dos pokemons del equipo del jugador. 
+    /// Entradas: GameObject interfazPokemonCambiar
+    /// Salidas: Ninguna
+    /// Precondiciones: interfazPokemonCambiar no debe estar a null(Sino se producira un NullPointerException)
+    /// Postcondiciones: Se intercambiaran la posicion dos pokemons del equipo del jugador, las interfaces que representan a los pokemons tambien intercambiaran su posicion.
+    /// </summary>
+    /// <param name="interfazPokemonCambiar"></param>
     public void aplicarCambioPosicionPokemons(GameObject interfazPokemonCambiar) {
         PokemonJugador pokemonCambiar = jugador.EquipoPokemon.Find(g => g.PokemonNumero == Int16.Parse(interfazPokemonCambiar.name));
 
@@ -117,15 +153,17 @@ public class MenuEquipoPokemon : MonoBehaviour
             indexPokemonCambiar = jugador.EquipoPokemon.IndexOf(pokemonCambiar),
             numeroEquipadoPokemonSeleccionado = pokemonSeleccionado.NumeroEquipado;
 
+        //Los objetos pokemons intercambian sus atributos numeroEquipado
         pokemonSeleccionado.NumeroEquipado = pokemonCambiar.NumeroEquipado;
         pokemonCambiar.NumeroEquipado = numeroEquipadoPokemonSeleccionado;
-
+        
+        //Se intercambiar en la lista EquipoPokemon del jugador
         jugador.EquipoPokemon[indexPokemonSeleccionado] = pokemonCambiar;
         jugador.EquipoPokemon[indexPokemonCambiar] = pokemonSeleccionado;
 
+        //Se realiza el intercambio de las interfaces de los pokemons
         GameObject contentPokemons = gameObject.transform.Find("ContentPokemons").gameObject,
-             interfazPokemonCambiarMenuEquipo;
-        interfazPokemonCambiarMenuEquipo = contentPokemons.transform.Find(pokemonCambiar.PokemonNumero.ToString()).gameObject;
+                   interfazPokemonCambiarMenuEquipo = contentPokemons.transform.Find(pokemonCambiar.PokemonNumero.ToString()).gameObject;
 
         Vector3 positionPokemonSeleccionado = interfazPokemonSeleccionado.transform.position;
         interfazPokemonSeleccionado.transform.position = interfazPokemonCambiarMenuEquipo.transform.position;
