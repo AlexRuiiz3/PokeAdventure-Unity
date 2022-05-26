3d429163d349d5a773d5aa8209b0b7e574daaa30
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-05-26 11:33:56 +0200

    Update UtilidadesSystemaBatalla.cs

--- Assets/Scripts/Utilidades/UtilidadesSystemaBatalla.cs
+++ Assets/Scripts/Utilidades/UtilidadesSystemaBatalla.cs
@@ -17,7 +17,6 @@ public class UtilidadesSystemaBatalla
             multiplicador = 2;
             textoMostrarResultado.text = $"Es supereficaz!";
         }
-
         return multiplicador;
     }
 
@@ -31,7 +30,6 @@ public class UtilidadesSystemaBatalla
     public static int incrementarDanhoMovimientoPorCritico(int danho, int probabilidadCritico, string nombrePokemon, string nombreMovimiento, TextMeshProUGUI textoMostrarResultado)
     {
         int aleatorioCritico = Random.Range(1, 10);
-
         if (aleatorioCritico <= probabilidadCritico)
         {
             textoMostrarResultado.text = $"{nombrePokemon} ha usado {nombreMovimiento}, golpe critico!";
@@ -55,25 +53,33 @@ public class UtilidadesSystemaBatalla
     {
         int nivelPokemonRival,
             nivelMedioEquipoJugador = 0,
-            aletorioSubidaNivel = Random.Range(0, pokemonsJugador.Count + 1);
+            aletorioSubidaNivel = Random.Range(1, 3),//pokemonsJugador.Count + 1);
+            randomSubirNivelONo = Random.Range(1, 3);
+        
         foreach (PokemonJugador pokemonJugador in pokemonsJugador)
         {
             nivelMedioEquipoJugador += pokemonJugador.Nivel;
         }
         nivelMedioEquipoJugador /= pokemonsJugador.Count;
-
-        nivelPokemonRival = nivelMedioEquipoJugador + aletorioSubidaNivel;
-
+        
+        if(randomSubirNivelONo == 1){ //Si es 1 se subira el nivel del pokemon rival
+            nivelPokemonRival = nivelMedioEquipoJugador + aletorioSubidaNivel;
+            if(nivelPokemonRival > 100){
+                nivelPokemonRival = 100;
+            }
+        }else{
+            nivelPokemonRival = nivelMedioEquipoJugador - aletorioSubidaNivel;
+            if(nivelPokemonRival < 1){
+                nivelPokemonRival = 1;
+            }
+        }
         return nivelPokemonRival;
     }
 
     public static bool determinarCapturarPokemon(int indicePokeball,int psMaximos, int psActuales) {
         int random = Random.Range(1, 101);
-
         int numero = (3 * psMaximos - 2 * psActuales) * indicePokeball;
-
         numero /= (3 * psMaximos);
-
         return random <= numero;
     }
 }
