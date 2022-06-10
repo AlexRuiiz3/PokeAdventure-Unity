0776b3d67315562fe0b4a80e7786581d69187479
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-06-10 11:43:35 +0200

    Sumarys UtilidadesObjetosInteractables

--- Assets/Scripts/Utilidades/UtilidadesObjetosInteractables.cs
+++ Assets/Scripts/Utilidades/UtilidadesObjetosInteractables.cs
@@ -9,12 +9,19 @@ using System.Linq;
 
 public class UtilidadesObjetosInteractables
 {
-    public void determinarAccionFinDialogo() {// No es static porque sino no se podria usar StartCoroutine
+    /// <summary>
+    /// Cabecera: public static void determinarAccionFinDialogo() 
+    /// Comentario: Este metodo se encarga de determinar que operacion realizar en funcion del objeto con el que se haya interactuado.
+    /// Entradas: Ninguna
+    /// Salidas: Ninguna
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se realizara una operacion determinado en funcion del objeto con el que se haya interactuado.
+    /// </summary>
+    public static void determinarAccionFinDialogo() {
         string objetoInteraccion = PlayerPrefs.GetString("InteraccionConObjeto");
 
         switch (objetoInteraccion) {
-            case "Medico":
-                curarPokemonsJugador(); break;
+            case "Medico": curarPokemonsJugador(); break;
             case "Vendedor": activarMenuObjeto("MenuTienda"); break;
             case "PC": activarMenuObjeto("MenuPC"); break;
             case "Item":
@@ -24,15 +31,14 @@ public class UtilidadesObjetosInteractables
                 playerController.iniciarCoroutineAsignarObjetoEncontrado();
                 //StartCoroutine(playerController.asignarObjetoEncontrado()); //No se puede hacer aqui ya que esta clase no esta asociada a ningun gameObject del juego
                 break;
-            case "Trainer": case
-                "TrainerInteraccion":
+            case "Trainer": case "TrainerInteraccion":
                 UtilidadesEscena.destruirGameObjectEspecifico("AudioTemporal");
                 PlayerPrefs.SetString("NameNextScene", "BattleTrainerScene");
                 SceneManager.LoadScene("BattleTrainerScene", LoadSceneMode.Additive);
                 break;
         }
     }
-
+    //Metodo que cura los pokemons del jugador
     private void curarPokemonsJugador() {
         UtilidadesEscena.llamarActivarAudioMomentaneo("Iteracion/Recovery", 3f);
         Jugador jugador = GameObject.Find("Player").GetComponent<PlayerController>().Jugador;
@@ -41,6 +47,7 @@ public class UtilidadesObjetosInteractables
             pokemon.HP = pokemon.HPMaximos;
         }
     }
+    //Metodo que activa un menu en especifico, determinado por el parametro recibido 
     private void activarMenuObjeto(string nombreMenu) {
         GameObject menu = Resources.FindObjectsOfTypeAll<GameObject>().First(g => g.name == nombreMenu);
         UtilidadesEscena.activarDesactivarMenuYTiempoJuego(menu);
