f81b24f59b8348f496416f4c1c6a1e4f8839d837
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-06-10 09:56:52 +0200

    Update GestoraPokemonsJugadorBL.cs

--- Assets/Scripts/BBDD/BL/Gestora/GestoraPokemonsJugadorBL.cs
+++ Assets/Scripts/BBDD/BL/Gestora/GestoraPokemonsJugadorBL.cs
@@ -4,10 +4,20 @@ using System.Collections.Generic;
 
 public class GestoraPokemonsJugadorBL
 {
+    /// <summary>
+    /// Cabecera: public static void guardarPokemonsDeJugador(int idJugador, List<PokemonJugador> pokemons) 
+    /// Comentario: Este metodo se encarga de llamar al metodo guardarPokemonsDeJugador de la clase GestoraPokemonsJugadorDAL de la capa DAL.
+    /// Entradas: int idJugador, List<PokemonJugador> pokemons
+    /// Salidas: Ninguna
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se insertaran, eliminaran o actualizaran los pokemons asociados a un jugador en la base de datos.
+    /// </summary>
+    /// <param name="idJugador"></param>
+    /// <param name="pokemons"></param>
+    /// <returns></returns>
     public static void guardarPokemonsDeJugador(int idJugador, List<PokemonJugador> pokemons)
     {
         GestoraPokemonsJugadorDAL.guardarPokemonsDeJugador(idJugador,pokemons);
     }
-
 }
 
