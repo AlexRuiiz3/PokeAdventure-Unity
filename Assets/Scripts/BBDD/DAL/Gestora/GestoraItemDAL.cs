9bd4cc406e3ea2db9b79ac33e5c49e5b18a81eff
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-06-10 09:41:40 +0200

    Sumary GestoraItemDAL

--- Assets/Scripts/BBDD/DAL/Gestora/GestoraItemDAL.cs
+++ Assets/Scripts/BBDD/DAL/Gestora/GestoraItemDAL.cs
@@ -8,7 +8,22 @@ using UnityEngine;
 
 public class GestoraItemDAL
 {
-    public static int actualizarItemsJugador(List<ItemConCantidad> items, int idJugador)
+    /// <summary>
+    /// Cabecera: public static int eliminarYActualizarItemsJugador(List<ItemConCantidad> items, int idJugador)
+    /// Comentario: Este metodo se encarga de eliminar de la base de datos los items que no se encuentren en una lista de items(Mochila, lista de items del jugador) y 
+    ///             añadir los items que no existan en la base de datos y si en la lista y actualizar aquellos que si existan.
+    /// Entradas: int idJugador, List<PokemonEncontrado> pokemonsEncontrados
+    /// Salidas: int inserciones
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se realizaran operaciones de manipulacion(Insert,Delete,Update) sobre los items de un jugador en la base de datos. 
+    ///                  No se realizara ninguna operacion en los siguientes casos:
+    ///                  1: Si el id del jugador no existe en la base de datos o no es valido, no se realiara ninguna operacion. 
+    ///                  2: Si la lista recibida esta vacia, no hay items                        
+    /// </summary>
+    /// <param name="items"></param>
+    /// <param name="idJugador"></param>
+    /// <returns>int</returns>
+    public static int eliminarYActualizarItemsJugador(List<ItemConCantidad> items, int idJugador)
     {
         int inserciones = 0;
         SqliteConnection conexion = null;
