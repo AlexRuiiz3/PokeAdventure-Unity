d34146a07a84eec3884bca79e22aea60871d8d36
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-06-10 12:46:25 +0200

    Delete AnimarGif.js

--- Assets/Scripts/Scripts/Escenas/AnimarGif.js
+++
@@ -1,7 +0,0 @@
-var frames : Texture2D[];
-var framesPorSegundo = 10.0;
-function Update () {
-var index : int = Time.time * framesPorSegundo;
-index = index % frames.Length;
-GetComponent.<Renderer>().material.mainTexture = frames[index];
-}
