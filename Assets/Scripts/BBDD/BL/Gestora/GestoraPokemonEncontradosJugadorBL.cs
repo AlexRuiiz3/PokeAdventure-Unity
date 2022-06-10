bd9f0ffd874a84c4b510405e7917ec669221c5ac
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-06-10 09:54:43 +0200

    Update GestoraPokemonEncontradosJugadorBL.cs

--- Assets/Scripts/BBDD/BL/Gestora/GestoraPokemonEncontradosJugadorBL.cs
+++ Assets/Scripts/BBDD/BL/Gestora/GestoraPokemonEncontradosJugadorBL.cs
@@ -8,13 +8,13 @@ public class GestoraPokemonEncontradosJugadorBL
     /// Cabecera: public static void insertarPokemonsEncontradosAJugador(int idJugador, List<PokemonEncontrado> pokemonsEncontrados) 
     /// Comentario: Este metodo se encarga de llamar al metodo insertarPokemonsEncontradosAJugador de la clase GestoraPokemonEncontradosJugadorDAL de la capa DAL.
     /// Entradas: int idJugador, List<PokemonEncontrado> pokemonsEncontrados
-    /// Salidas: void
+    /// Salidas: Ninguna
     /// Precondiciones: Ninguna
     /// Postcondiciones: Se insertara en la base de datos los pokemons encontrados asociados a un jugador.
     /// </summary>
-    /// <param name="id"></param>
-    /// <param name="dinero"></param>
-    /// <returns>int</returns>
+    /// <param name="idJugador"></param>
+    /// <param name="pokemonsEncontrados"></param>
+    /// <returns></returns>
     public static void insertarPokemonsEncontradosAJugador(int idJugador, List<PokemonEncontrado> pokemonsEncontrados) 
     {
         GestoraPokemonEncontradosJugadorDAL.insertarPokemonsEncontradosAJugador(idJugador, pokemonsEncontrados);
