ad76d6eb3085af97446d94523cbdfff9fea49bdb
Alejandro Ruiz Campos <60139737+AlexRuiiz3@users.noreply.github.com>
2022-06-10 09:51:34 +0200

    Sumary GestoraJugadorBL

--- Assets/Scripts/BBDD/BL/Gestora/GestoraJugadorBL.cs
+++ Assets/Scripts/BBDD/BL/Gestora/GestoraJugadorBL.cs
@@ -18,6 +18,18 @@ public class GestoraJugadorBL
     {
         GestoraJugadorDAL.insertarJugador(jugador);
     }
+    
+    /// <summary>
+    /// Cabecera: public static int actualizarDineroJugador(int id, int dinero)
+    /// Comentario: Este metodo se encarga de llamar al metodo actualizarDineroJugador de la clase GestoraJugadorDAL de la capa DAL.
+    /// Entradas: int id, int dinero
+    /// Salidas: int
+    /// Precondiciones: Ninguna
+    /// Postcondiciones: Se insertara en la base de datos un nuevo jugador. Si se produce algun tipo de excepcion el jugador no se einsertara en la base de datos.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="dinero"></param>
+    /// <returns>int</returns>
     public static int actualizarDineroJugador(int id, int dinero) {
         return GestoraJugadorDAL.actualizarDineroJugador(id,dinero);
     }
